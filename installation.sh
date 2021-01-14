93a98be0dd54b44752ca008696a994e4df58ff08
Franrossi97 <43619695+Franrossi97@users.noreply.github.com>
2021-01-13 22:26:20 -0300

    Installation file

---
+++ installation.sh
@@ -0,0 +1,27 @@
+sudo apt update
+
+sudo apt install npm
+
+npm install -g @angular/cli
+
+ng new Ensolvers_exercise_frontend
+
+git clone https://github.com/Franrossi97/Ensolvers_exercise_frontend
+
+cd Ensolvers_exercise_frontend
+
+cd ..
+
+sudo apt-get install mysql-server=10.4.14
+
+git clone https://github.com/Franrossi97/Ensolvers_exercise_backend
+
+cd Ensolvers_exercise_backend
+
+mysql> todo_list_app.sql
+
+cd ..
+
+cd Ensolvers_exercise_frontend
+
+ng serve
